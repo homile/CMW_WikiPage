2305bf50e5c1d2bc2bc3e242fd1ba12dc9cb2cb1
homile <ssanho2@naver.com>
2024-02-10 17:51:42 +0900

    feat: 위키 본문에 제목 자동링크 기능 추가

--- src/app/wiki/[wikiId]/page.tsx
+++ src/app/wiki/[wikiId]/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 
 // import api
-import { getWiki } from "@/app/api/wikis";
+import { getWiki, getWikiList } from "@/app/api/wikis";
 
 // import types
 import { Wiki } from "@/app/types/wiki";
@@ -12,6 +12,7 @@ import styles from "./wikiPage.module.css";
 
 const WikiPage = ({ params }: { params: { wikiId: string } }) => {
   const [wikiInfo, setWikiInfo] = useState<Wiki>();
+  const [wikiList, setWikiList] = useState<Wiki[]>([]);
 
   // 위키정보 가져오는 함수
   const fetchWiki = async () => {
@@ -23,14 +24,36 @@ const WikiPage = ({ params }: { params: { wikiId: string } }) => {
     }
   };
 
+  // 위키리스트 가져오는 함수
+  const fetchWikiList = async () => {
+    try {
+      const data = await getWikiList();
+      setWikiList(data.data);
+    } catch (error) {
+      console.error("위키리스트를 가져오지 못했습니다.", error);
+    }
+  };
+
+  // 위키본문 출력 함수
+  const handleWikiTitleLink = (content: string) => {
+    wikiList.forEach((wiki: Wiki) => {
+      const link = `<a href="/wiki/${wiki.id}" style="color: blue;">${wiki.title}</a>`;
+      const regExp = new RegExp(wiki.title, "g");
+      content = content.replace(regExp, link);
+    });
+
+    return <p className={styles.content} dangerouslySetInnerHTML={{ __html: content }} />;
+  };
+
   useEffect(() => {
     fetchWiki();
+    fetchWikiList();
   }, []);
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{wikiInfo?.title}</h1>
-      <p className="content">{wikiInfo?.content}</p>
+      {wikiInfo && handleWikiTitleLink(wikiInfo.content)}
     </div>
   );
 };
