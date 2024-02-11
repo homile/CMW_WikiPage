b269b449a6724b0a64610a0a32defa63fa716885
homile <ssanho2@naver.com>
2024-02-11 19:30:43 +0900

    fix: mock data 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,9 +6,9 @@ import { Wiki } from "@/app/types/wiki";
 let wikiList: Wiki[] = [
   { id: "1", title: "위키제목1", content: "위키본문1" },
   { id: "2", title: "위키제목2", content: "위키본문2" },
-  { id: "3", title: "위키제목3", content: "위키본문3" },
+  { id: "3", title: "위키본문에 제목이 추가된 위키1", content: "위키본문3 위키제목1 위키제목2" },
   { id: "4", title: "위키제목4", content: "위키본문4" },
-  { id: "5", title: "위키제목5", content: "위키본문5 위키제목2" },
+  { id: "5", title: "위키본문에 제목이 추가된 위키2", content: "위키본문5 위키제목2" },
   { id: "6", title: "위키제목6", content: "위키본문6" },
 ];
 
